38554f60463b59a9bf9d86c7385790680295cdde
fxwiegand <fxwiegand@wgdnet.de>
2025-06-10 11:25:48 +0200

    Deploy report

--- S02/functions.js
+++ S02/functions.js
@@ -8,28 +8,6 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'c1') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'cfh (c3)') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
  } catch (e) { datavzrd.custom_error(e, 'sp') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
@@ -41,7 +19,7 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'cfh (c1)') }}
+ } catch (e) { datavzrd.custom_error(e, '5') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -63,18 +41,7 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'c3') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'cre') }}
+ } catch (e) { datavzrd.custom_error(e, 'OF') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -96,7 +63,7 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'bip') }}
+ } catch (e) { datavzrd.custom_error(e, 'cfh (c3)') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -107,7 +74,7 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'c2.1') }}
+ } catch (e) { datavzrd.custom_error(e, 'NF') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -129,7 +96,18 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, '3') }}
+ } catch (e) { datavzrd.custom_error(e, 'c3') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'bip') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -162,61 +140,6 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'cfs (c2)') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'NF') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'CMF') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'OF') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'imp') }}
-function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "1") {
-      return trueLabel;
-  } else if (value === "0") {
-      return falseLabel;
-  } else {
-      return value;
-  }
  } catch (e) { datavzrd.custom_error(e, 'icp') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
@@ -228,7 +151,7 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, 'c2') }}
+ } catch (e) { datavzrd.custom_error(e, 'c2.1') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -250,7 +173,29 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, '4') }}
+ } catch (e) { datavzrd.custom_error(e, 'cfh (c1)') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'c1') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'c2') }}
 function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
   const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
   const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
@@ -272,4 +217,59 @@ function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try {
   } else {
       return value;
   }
- } catch (e) { datavzrd.custom_error(e, '5') }}
+ } catch (e) { datavzrd.custom_error(e, '4') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'imp') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'cfs (c2)') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'CMF') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'cre') }}
+function custom_func_e07aaf4ac8f1c8565ab9113622396cc5(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "1") {
+      return trueLabel;
+  } else if (value === "0") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, '3') }}
